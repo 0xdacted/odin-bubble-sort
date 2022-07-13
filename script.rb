4365b0682cd340483765d07d61f9a55da573f20b
Red <crabmarket56@gmail.com>
2022-07-13 15:00:30 -0400

    Save draft logic

--- script.rb
+++ script.rb
@@ -1,19 +1,28 @@
 # for each element in the list, look at the element and the element directly to its right
 # if out of order swap the elements
 require 'pry-byebug'
-def bubble_sort(array)
 
+def bubble_sort(array)
+  
 array.each_with_index do |element, index|
   original_index = index
+  
   until index >= array.length do
-    index += 1
-
+    
     if array[original_index].to_i > array[index].to_i do
-      array[original_index], array[index] = array[index], array[original_index]
+      swap(array[original_index].to_i, array[index].to_i)
+    
+      index += 1
 end
 end
 end
 p array
 end
 
-bubble_sort([4,3,78,2,0,2])
+def swap!(a,b)
+  self[a], self[b] = self[b], self[a]
+self
+end
+
+
+array = bubble_sort([4,3,78,2,0,2])
