45a235d121c3568f6c5f7e1a5a37d019cb93f5f4
Red <crabmarket56@gmail.com>
2022-07-13 14:36:28 -0400

    Revised draft logic

--- script.rb
+++ script.rb
@@ -1,23 +1,19 @@
 # for each element in the list, look at the element and the element directly to its right
 # if out of order swap the elements
-
+require 'pry-byebug'
 def bubble_sort(array)
-sorted = []
-  array.each_with_index do |value1, index1|
 
-  array.each_with_index do |value2, index2|
-    
-   if value1 > value2 
-      array[index1] = value2
-      array[index2] = value1
-    
-   elsif value2 > value1
-      array[index1] = value1
-      array[index2] = value2
-   end
+array.each_with_index do |element, index|
+  original_index = index
+  until index >= array.length do
+    index += 1
+
+    if array[original_index].to_i > array[index].to_i do
+      array[original_index], array[index] = array[index], array[original_index]
+end
 end
 end
 p array
 end
 
-array = [4,3,78,2,0,2]
+bubble_sort([4,3,78,2,0,2])
